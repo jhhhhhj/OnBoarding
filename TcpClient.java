3dcee0d409143a28879c1d8d682dbd610354b845
jhhhhhj <104751668+jhhhhhj@users.noreply.github.com>
2024-02-13 22:39:55 +0900

    Add files via upload

---
+++ TcpClient.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TcpClient {
+    public static void main(String[] args) {
+        String serverHost = "localhost"; // 서버 호스트 주소
+        int serverPort = 8080; // 서버 포트
+
+        try (
+                Socket socket = new Socket(serverHost, serverPort);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("Server response: " + in.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
